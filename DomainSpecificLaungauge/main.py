528d31ec691af252eea4192e4d61c3ee6142ad77
Epein5 <aayushgtm5@gmail.com>
2023-09-22 23:06:53 +0545

    DSL

---
+++ DomainSpecificLaungauge/main.py
@@ -0,0 +1,38 @@
+import ply.lex as lex
+import ply.yacc as yacc
+
+# Lexer tokens
+tokens = (
+    'OPERATION',
+    'NUMBER',
+)
+
+t_OPERATION = r'SUM|STRSUM'
+t_NUMBER = r'\d+'
+
+t_ignore = ' \t\n'
+
+def t_error(t):
+    print(f"Invalid character: '{t.value[0]}'")
+    t.lexer.skip(1)
+
+lexer = lex.lex()
+
+def p_expression_operation(p):
+    '''expression : OPERATION NUMBER NUMBER'''
+    if p[1] == 'SUM':
+        p[0] = int(p[2])+int(p[3])
+    elif p[1] == 'STRSUM':
+        p[0] = p[2] + p[3]
+
+def p_error(p):
+    print("Syntax error")
+
+parser = yacc.yacc()
+
+input_text = """
+    SUM 2 3
+    """
+
+result = parser.parse(input_text)
+print("Result:", result)
